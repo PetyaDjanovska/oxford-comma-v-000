b58d828176c1fa14bb30f7ff85b06ec497924b2a
Petya Djanovska <petya.djanovska@gmail.com>
2017-12-29 01:50:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 1
     array
   elsif
-    string = String.New
+    string = String.new
     string << array[0...array.length-1].join(", ")
     string << array[-1].join("and ")
     string
