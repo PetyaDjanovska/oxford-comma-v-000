ff0a10b45dadb1dc3d887285a0bbdad5e60a7d4e
Petya Djanovska <petya.djanovska@gmail.com>
2017-12-29 01:56:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   string = String.new
   if array.size == 1
     string = array.join
+  elsif array.size == 2
+    string = array.join("and")
   elsif
     string << array[0...array.length-1].join(", ")
     string << array[-1].join("and")
